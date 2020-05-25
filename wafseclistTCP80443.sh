3a5652ff1d49709a539f3ef53c7bab2238598064
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-05-25 10:53:14 +0200

    Update wafseclistTCP80443.sh

--- wafseclistTCP80443.sh
+++ wafseclistTCP80443.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-rm -f wafrule-TCP80443.sh
+rm -f wafrule-TCP80.sh
 rm -f wafrule-TCP443.sh
-wget https://raw.githubusercontent.com/BaptisS/oci_waf_seclist/master/wafrule-TCP80443.sh
+wget https://raw.githubusercontent.com/BaptisS/oci_waf_seclist/master/wafrule-TCP80.sh
 wget https://raw.githubusercontent.com/BaptisS/oci_waf_seclist/master/wafrule-TCP443.sh
-chmod +x wafrule-TCP80443.sh
+chmod +x wafrule-TCP80.sh
 chmod +x wafrule-TCP443.sh
 
 wafips=$(oci waas edge-subnet list --all)
@@ -15,7 +15,7 @@ rm -f seclist-waf-TCP443.json
 
 
 echo "[" >> seclist-waf-TCP80443-temp.json
-for cidr in $wafcidrs; do ./wafrule-TCP80443.sh $cidr ; done
+for cidr in $wafcidrs; do ./wafrule-TCP80.sh $cidr ; done
 for cidr in $wafcidrs; do ./wafrule-TCP443.sh $cidr ; done
 cat seclist-waf-TCP443.json >> seclist-waf-TCP80443-temp.json
 echo "]" >> seclist-waf-TCP80443-temp.json
@@ -25,9 +25,9 @@ rm -f seclist-waf-TCP80443-temp.json
 oci network security-list update --security-list-id $wafseclist --ingress-security-rules file://seclist-waf-TCP80443.json --force
 
 
-rm -f wafrule-TCP80443.sh
+rm -f wafrule-TCP80.sh
 rm -f wafrule-TCP443.sh
 
 rm -f seclist-waf-TCP80443-temp.json
 rm -f seclist-waf-TCP80443.json
-#rm -f seclist-waf-TCP443.json
+rm -f seclist-waf-TCP443.json
